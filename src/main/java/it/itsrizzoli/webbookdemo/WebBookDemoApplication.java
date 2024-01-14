876fa60bcde167e8a34eeadb8244fec178028352
GiacomoVilla14 <g.villa@itsrizzoli.it>
2024-01-14 15:18:34 +0100

    Creazione file e creazione cartella Controller

---
+++ src/main/java/it/itsrizzoli/webbookdemo/WebBookDemoApplication.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.webbookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBookDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itsrizzoli/webbookdemo/WebBookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.webbookdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
