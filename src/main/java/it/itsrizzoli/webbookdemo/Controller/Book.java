4f89381a76d99e52ef4643c30933f7104ecdf7c2
GiacomoVilla14 <g.villa@itsrizzoli.it>
2024-01-14 15:55:32 +0100

    Modifiche alle variabili della classe Book

--- src/main/java/it/itsrizzoli/webbookdemo/Controller/Book.java
+++ src/main/java/it/itsrizzoli/webbookdemo/Controller/Book.java
@@ -1,5 +1,7 @@
 package it.itsrizzoli.webbookdemo.Controller;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,14 +14,25 @@ public class Book {
     @Size(min = 2, max = 50)
     String autore;
 
-    @Size(min = 0, max = 30)
-    String descrizione;
+    @Min(1900)
+    @Max(2024)
+    int annoPubblicazione;
 
-    public Book(String titolo, String autore, String descrizione) {
+    @Min(5)
+    @Max(100)
+    int prezzo;
+
+    public Book(String titolo, String autore, int annoPubblicazione, int prezzo) {
         this.titolo = titolo;
         this.autore = autore;
-        this.descrizione = descrizione;
+        this.annoPubblicazione = annoPubblicazione;
+        this.prezzo = prezzo;
     }
+
+    public Book(String titolo, String autore, int annoPubblicazione) {
+
+    }
+
 
     public String getTitolo() {
         return titolo;
@@ -37,11 +50,19 @@ public class Book {
         this.autore = autore;
     }
 
-    public String getDescrizione() {
-        return descrizione;
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
     }
 
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
     }
 }
